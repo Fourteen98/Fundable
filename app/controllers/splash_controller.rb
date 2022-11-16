0efda20cf192eca3cb0b28e49bc24415311e7f2c
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-11-16 12:55:22 +0000

    configure tailwind

---
+++ app/controllers/splash_controller.rb
@@ -0,0 +1,5 @@
+class SplashController < ApplicationController
+  def index
+    @logo = "logo-1.png"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  get 'splash/index'
   get 'users/index'
   get 'users/show'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "users#index"
+  root "splash#index"
   resources :users, only: %i[index show]
 end
