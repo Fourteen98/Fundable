60073ae09e0c2ec2fd452c4a09a8dbab094a9a0e
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-11-16 12:47:05 +0000

    devise working good

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "users#index"
+  resources :users, only: %i[index show]
 end
