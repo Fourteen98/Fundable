ca97ccf57815e2641c99133bdf60c86a03d92a8b
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-11-18 15:59:02 +0000

    add fetch and sum of groups purchases '

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,6 +2,11 @@ class GroupsController < ApplicationController
   # before_action :authenticate_user!
   def index
     @groups = Group.where(author_id: current_user.id).order('created_at DESC')
+    # get total amount of purchases for each group
+    @total_amount = {}
+    @groups.each do |group|
+      @total_amount[group.name] = Purchase.joins(:group_purchases).where(group_purchases: { group_id: group.id }).where(author_id: current_user.id).sum(:amount)
+    end
   end
 
   def new
--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,11 +1,30 @@
 class PurchasesController < ApplicationController
   def index
-    @purchases = GroupPurchase.where(group_id: params[:group_id])
+    p params[:group_id]
+    @group = Group.find_by(id: params[:group_id])
+    @purchases = Purchase.joins(:group_purchases).where(group_purchases: { group_id: params[:group_id] }).where(author_id: current_user.id).order('created_at DESC')
+    @total_amount = Purchase.joins(:group_purchases).where(group_purchases: { group_id: params[:group_id] }).where(author_id: current_user.id).sum(:amount)
+      # Purchase.includes(:group_purchases).where(author_id: current_user.id, group_purchases: {group_id: params[:id]}).sum(:amount)
   end
 
   def new
+    @purchase = Purchase.new
   end
 
   def create
+    @purchase = Purchase.new(purchase_params)
+    @purchase.author_id = current_user.id
+    if @purchase.save
+      redirect_to purchases_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def purchase_params
+    params.require(:purchase).permit(:name, :amount)
   end
 end
